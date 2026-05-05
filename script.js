da554aae8d79b798912b204132f6e800f81ca95d
rqndykss <gaming12wer09@gmail.com>
2026-05-05 23:47:45 +0300

    add script, add animation +section (hover effect/active)

---
+++ script.js
@@ -0,0 +1,12 @@
+const elements = document.querySelectorAll(`.card__wrapper`);
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+elements.forEach(el => observer.observe(el));
